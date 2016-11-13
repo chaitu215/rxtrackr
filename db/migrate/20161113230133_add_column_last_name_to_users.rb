1e5a0f149e8e5d3e690ff6652064f2a616440468
Rebecca <beckysherman000@gmail.com>
2016-11-13 18:19:26 -0500

    Add attributes to users table

---
+++ db/migrate/20161113230133_add_column_last_name_to_users.rb
@@ -0,0 +1,5 @@
+class AddColumnLastNameToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :last_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113223241) do
+ActiveRecord::Schema.define(version: 20161113231826) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20161113223241) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "role",                   default: 1
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "birthdate"
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
