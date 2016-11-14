ee4db027e237c0b1553555a7f4b5af6eb09a8b68
Rebecca <beckysherman000@gmail.com>
2016-11-14 15:32:35 -0500

    Add static pages controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root   'static_pages#home'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
+  root 'static_pages#home'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to:'static_pages#contact'
 end
