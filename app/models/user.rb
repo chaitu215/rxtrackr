4d7d6b620d87658479b266564ca3f2f8c56e5051
Rebecca <beckysherman000@gmail.com>
2016-11-15 09:44:23 -0500

    Add custom fields to Devise registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
 
   before_save   :downcase_email
 
+  validates :first_name, presence: true, length: { maximum: 40 }
   validates :email,      presence: true, uniqueness: true
   validates :password,   presence: true, length: { minimum: 6 }, allow_nil: true
 
