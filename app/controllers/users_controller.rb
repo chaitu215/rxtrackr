fab15e0d7a135dfd422ddad08ed0ef9a696a5456
Rebecca <beckysherman000@gmail.com>
2017-01-13 09:45:28 -0500

    Add current_user landing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_user,           only: [:index, :show]
 
   def index
   end
 
   def show
-    @user = current_user
   end
+
+  private
+
+    def set_user
+      @user = current_user
+    end
 end
