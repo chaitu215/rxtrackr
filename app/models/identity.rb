daa82eabf55189c773d9117f393b49f8e3e9de73
Rebecca <beckysherman000@gmail.com>
2017-01-23 11:44:42 -0500

    Add Identity model and methods and edit to manage multiple sign-ins for same user

---
+++ app/models/identity.rb
@@ -0,0 +1,33 @@
+class Identity < ActiveRecord::Base
+  belongs_to :user
+
+  def self.find_for_facebook_oath(auth)
+    user = Identity.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user.nil?
+      user.user
+    else
+      registered_user = User.where(:email => auth.info.email).first
+      unless registered_user.nil?
+        Identity.create!(
+                          provider: auth.provider,
+                          uid:      auth.uid,
+                          user_id:  registered_user.id
+                        )
+        registered_user
+      else
+        user = User.create!(
+                          email:      auth.info.email,
+                          first_name: auth.info.first_name,
+                          last_name:  auth.info.last_name,
+                          password:   Devise.friendly_token[0,20]
+                          )
+        identity = Identity.create!(
+                          provider: auth.provider,
+                          uid:      auth.uid,
+                          user_id:  registered_user.id
+                          )
+        user
+      end
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,26 +16,6 @@ class User < ApplicationRecord
 
   after_initialize :set_default_role
 
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
-      user.password = Devise.friendly_token[0,20]
-      user.save
-    end
-  end
-
   private
 
     def set_default_role
