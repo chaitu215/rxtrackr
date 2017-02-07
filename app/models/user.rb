513717de5cc732d0880472a73e28cd939b494cd2
Rebecca <beckysherman000@gmail.com>
2017-02-07 15:04:07 -0500

    Make minor wording change - allergic symptoms

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,17 +13,19 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true, length: { maximum: 40 }
   validates :email,      presence: true, uniqueness: true
-  validates :password,   presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password,   presence: true, length: { minimum: 8 }, allow_nil: true
 
   enum role: [:normal, :admin]
 
   after_initialize :set_default_role
 
   def allergies_attributes=(allergies_attributes)
-    if allergies_attributes[:name].present?
-      allergy = Allergy.find_or_create_by(attribute)
-      if !self.user_allergies.include?(attribute[:name])
-        self.user_allergies.build(allergy_id: allergy.id)
+    allergies_attributes.values.each do |attribute|
+      if attribute[:name].present?
+        allergy = Allergy.find_or_create_by(attribute)
+        if !self.user_allergies.include?(attribute[:name])
+          self.user_allergies.build(allergy_id: allergy.id)
+        end
       end
     end
   end
